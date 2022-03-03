e6df7c55342fa4f80edbc0c1a37d89dd90c55523
Artur Lansoni <arturlansoni@gmail.com>
2022-03-03 19:41:27 -0300

    chore: setup react-toastfy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { toast } from "react-toastify";
 import Router from "./router";
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
+
+toast.configure();
 
 ReactDOM.render(
   <React.StrictMode>
