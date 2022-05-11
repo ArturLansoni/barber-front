766eb38a2cecdc07a6014b32063071981ee3990a
Artur Lansoni <arturlansoni@gmail.com>
2022-05-11 14:49:07 -0300

    feat: create barbers item

--- src/pages/barber-pages/barber-home/barber-home.jsx
+++ src/pages/barber-pages/barber-home/barber-home.jsx
@@ -1,89 +1,45 @@
-import React, { useEffect } from "react";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
-import { Button, Spinner } from "../../../components";
-import { ServiceItem } from "./sub-components";
-import { BarberHomeProvider, useBarberHome } from "./barber-home-context";
+import React from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { BarberHomeProvider } from "./barber-home-context";
 import "./barber-home-styles.css";
 
+const ServiceItem = ({ title, description, imageUrl, onPress = () => {} }) => {
+  return (
+    <Card
+      onClick={onPress}
+      sx={{ height: 120, margin: 5, display: "flex", flexDirection: "row" }}
+    >
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        sx={{ height: 120, width: 120 }}
+        image={imageUrl}
+        alt={title}
+      />
+    </Card>
+  );
+};
+
 const BarberHomePage = () => {
-  const {
-    state,
-    findServices,
-    onLogOut,
-    onOpenDeleteConfirmDialog,
-    onCloseDialog,
-    onDelete,
-    onCreateService,
-    onEditService,
-  } = useBarberHome();
-
-  useEffect(() => {
-    findServices();
-  }, []);
-
   return (
     <div className="home-page-container">
-      <header>
-        <h1>Barberbook</h1>
-        <Button color="error" onClick={onLogOut}>
-          LOGOUT
-        </Button>
-      </header>
-
-      <div className="home-title">
-        <h2>Serviços</h2>
-        <Button type="button" color="secondary" onClick={onCreateService}>
-          + Novo Serviço
-        </Button>
-      </div>
-
-      <ul>
-        {!state.services.length && state.isLoading && <Spinner />}
-        {state.services.map((i) => (
-          <ServiceItem
-            isLoading={state.isLoading}
-            key={i._id}
-            price={i.price}
-            description={i.description}
-            image={i.image}
-            estimatedTime={i.estimatedTime}
-            onEdit={() => onEditService(i)}
-            onDelete={() => onOpenDeleteConfirmDialog(i._id, i.description)}
-          />
-        ))}
-      </ul>
-
-      <Dialog
-        open={state.dialog.open}
-        onClose={onCloseDialog}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">Remover serviço</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {`Você tem certeza que deseja excluir ${state.dialog.description}?`}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onCloseDialog} variant="outlined" color="error">
-            Cancelar
-          </Button>
-          <Button
-            onClick={() => onDelete(state.dialog.id)}
-            autoFocus
-            color="secondary"
-          >
-            Excluir
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <ServiceItem
+        title="Barbearia um"
+        description="Teste de descricao, texto para testes, texto para testes, texto para testes, texto para testes, texto para testes"
+        imageUrl="https://blog.mensmarket.com.br/wp-content/uploads/2018/01/barbearia-barber-shop.jpg"
+      />
+      <Card className="bottom-container">
+        <Typography variant="body2" color="text.secondary">
+          teste
+        </Typography>
+      </Card>
     </div>
   );
 };
